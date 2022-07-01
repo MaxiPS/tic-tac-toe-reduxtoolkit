cf5142b27ecb45c5702eb9210fd0ccba9e4b5dd8
MaxiPS <usr2254@salesianos-lacuesta.net>
2022-07-01 14:21:43 +0100

    added functionality to Board component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,18 +1,135 @@
+import { useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getBoard,
+  getWinner,
+  getPlayer,
+  getTurnsCounter,
+} from "../../store/slices/board/selectors";
+import {
+  setBoard,
+  setWinner,
+  setPlayer,
+  setTurnsCounter,
+} from "../../store/slices/board/boardSlice";
+import { game } from "./tic-tac-toe";
 import "./board.css";
 
+
+
 export const Board = () => {
+  const dispatch = useDispatch();
+  const turnsCounter = useSelector(getTurnsCounter);
+  const board = useSelector(getBoard);
+  const winner = useSelector(getWinner);
+  const currentPlayer = useSelector(getPlayer);
+  const random = Math.floor(Math.random() * 2);
+
+  useEffect(() => {
+    if (currentPlayer === "") {
+      getRandomPlayer();
+    }
+    const checkGame = () => {
+      const result = game(board, turnsCounter);
+      if (result) {
+        dispatch(setWinner(result));
+      }
+    };
+    checkGame();
+
+    function getRandomPlayer() {
+      if (random !== 1) {
+        dispatch(setPlayer("X"));
+      } else {
+        dispatch(setPlayer("O"));
+      }
+    }
+  }, [board, turnsCounter, currentPlayer, random, dispatch]);
+
+  const handleGame = (e) => {
+    let index = e.target.id;
+    let newBoard = [...board];
+    if (winner) return;
+    if (newBoard[index] === "") {
+      newBoard[index] = currentPlayer;
+      dispatch(setBoard(newBoard));
+      swapPlayer();
+      dispatch(setTurnsCounter(turnsCounter + 1));
+    }
+  };
+
+  const swapPlayer = () => {
+    dispatch(setPlayer(currentPlayer === "X" ? "O" : "X"));
+  };
+
+  
+
   return (
     <>
       <div className="parent">
-        <div id="0" className="child top-row-position-left"></div>
-        <div id="1" className="child top-row-position-middle"></div>
-        <div id="2" className="child top-row-position-right"></div>
-        <div id="3" className="child middle-row-position-left"></div>
-        <div id="4" className="child middle-row-position-middle"></div>
-        <div id="5" className="child middle-row-position-right"></div>
-        <div id="6" className="child bottom-row-position-left"></div>
-        <div id="7" className="child bottom-row-position-middle"></div>
-        <div id="8" className="child bottom-row-position-right"></div>
+        <div
+          id="0"
+          onClick={handleGame}
+          className="child top-row-position-left"
+        >
+          {board[0]}
+        </div>
+        <div
+          id="1"
+          onClick={handleGame}
+          className="child top-row-position-middle"
+        >
+          {board[1]}
+        </div>
+        <div
+          id="2"
+          onClick={handleGame}
+          className="child top-row-position-right"
+        >
+          {board[2]}
+        </div>
+        <div
+          id="3"
+          onClick={handleGame}
+          className="child middle-row-position-left"
+        >
+          {board[3]}
+        </div>
+        <div
+          id="4"
+          onClick={handleGame}
+          className="child middle-row-position-middle"
+        >
+          {board[4]}
+        </div>
+        <div
+          id="5"
+          onClick={handleGame}
+          className="child middle-row-position-right"
+        >
+          {board[5]}
+        </div>
+        <div
+          id="6"
+          onClick={handleGame}
+          className="child bottom-row-position-left"
+        >
+          {board[6]}
+        </div>
+        <div
+          id="7"
+          onClick={handleGame}
+          className="child bottom-row-position-middle"
+        >
+          {board[7]}
+        </div>
+        <div
+          id="8"
+          onClick={handleGame}
+          className="child bottom-row-position-right"
+        >
+          {board[8]}
+        </div>
       </div>
     </>
   );
